7df3b4576aa4cbf07778a859daf763e3027872ba
Martina Peic Gavran <m.peicgavran@xximo.com>
2025-08-07 11:18:29 +0200

    setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styling/main.scss";
-import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </StrictMode>
 );
